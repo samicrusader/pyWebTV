aee14eaa8f9de5143cde8e1176677d1d7edbad16
samicrusader <hi@samicrusader.me>
2022-03-17 05:21:10 -0400

    add geoip based time resolving

--- pywebtv/functions.py
+++ pywebtv/functions.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 
+from datetime import datetime
+from geoip2 import database as geoip2
 import json
 import magic
 import os
+import pathlib
 from .decorators import WTVPResponse
 
 def load_json(file:str):
@@ -63,3 +66,27 @@ def return_service(name:str,
         constr = f' connections={connections}'
     
     return f'name={name} host={host} port={port}{strflags}{constr}'
+
+def returnLocalTime(ip:str):
+    """
+    Returns a dictionary of headers that will set the client's time.
+    This will work with a DB in MaxMind DB format. I use GeoIP2 for this example.
+    You will want to put that in the directory of this module.
+
+    If an error occurs with the DB, it will default to using the local machine's
+    timezone.
+    """
+    path = pathlib.Path(__file__).parent.resolve()
+    path = os.path.join(path, 'GeoIP2-City.mmdb')
+    with geoip2.Reader(path) as reader:
+        try:
+            r = reader.city(ip)
+        except:
+            tz = get_localzone()
+        else:
+            tz = r.location.time_zone
+    reader.close()
+    dt = datetime.now(tz)
+    return {'wtv-client-time-zone': dt.strftime('%Z %z'),
+            'wtv-client-time-dst-rule': dt.strftime('%Z'),
+            'wtv-client-date': dt.strftime("%a %b %d %H:%M:%S %Y")}
